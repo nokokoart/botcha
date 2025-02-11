7c43f39ea182cfff267cc9d5a8c4b6b40b32e613
nokokoart <161035669+nokokoart@users.noreply.github.com>
2025-02-11 10:18:44 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.apiConfig = {
+    apiKey: "Bearer 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",
+    referer: "https://nokokoart.com",
+    title: "Nokoko Art",
+    apiUrl: "https://openrouter.ai/api/v1/chat/completions"
+};
