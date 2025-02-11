acd35d19c3d18f7834f1429d2ed0a59ba8804264
nokokoart <161035669+nokokoart@users.noreply.github.com>
2025-02-11 10:59:02 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.apiConfig = {
-    apiKey: "Bearer 
-        ",
-    referer: "https://nokokoart.com",
-    title: "Nokoko Art",
-    apiUrl: "https://openrouter.ai/api/v1/chat/completions"
-};
