3bc1627384547dab823102fe53ef83cf78fe8730
nokokoart <161035669+nokokoart@users.noreply.github.com>
2025-02-11 10:58:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.apiConfig = {
-    apiKey: "Bearer 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",
+    apiKey: "Bearer 
+        ",
     referer: "https://nokokoart.com",
     title: "Nokoko Art",
     apiUrl: "https://openrouter.ai/api/v1/chat/completions"
